cb0dcd8bcc9cad5cdebd4aa671be968de69628b7
Murray <28TMurray@live.ytech.edu>
2025-03-13 22:53:15 -0400

    remove unnecessary files

--- js/about.js
+++
@@ -1,9 +0,0 @@
-const randomizerProject = document.getElementById("randomizer")
-const tierlistProject = document.getElementById("tierlist")
-const anyboardProject = document.getElementById("anyboard")
-
-console.log(getFileContent('programming-projects/randomizer.py'))
-
-async function getFileContent(url) {
-    return await fetch(url).then((res) => res.text()).then().catch((e) => console.error(e));
-}
